b100ba23f79ee60f6bccf639b71f447e6882186d
gyezekyan13 <gyezekyan13@gmail.com>
2024-02-20 00:39:01 +0400

    initial

---
+++ SqsPublisher/CustomerCreated.cs
@@ -0,0 +1,8 @@
+﻿namespace SqsPublisher;
+
+public class CustomerCreated
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var client = new AmazonSQSClient(RegionEndpoint.USEast1);
+
+var queueUrlResponse = await client.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(new CustomerCreated
+    {
+        Email = "test@gmail.com",
+        Id = Guid.NewGuid(),
+        Name = "Gagooo"
+    })
+};
+
+var response = await client.SendMessageAsync(sendMessageRequest);
+
+Console.ReadKey();
