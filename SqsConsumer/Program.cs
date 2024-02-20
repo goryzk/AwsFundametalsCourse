3a92a6f166c6bf295e03206c3a852b2ef381ee2e
gyezekyan13 <gyezekyan13@gmail.com>
2024-02-20 14:21:09 +0400

    added new project

--- SqsConsumer/Program.cs
+++ SqsConsumer/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 
-Console.WriteLine("Hello, World!");
+var cts = new CancellationTokenSource();
+var sqsClient = new AmazonSQSClient(RegionEndpoint.USEast1);
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string>{ "ALL" },
+    MessageAttributeNames = new List<string>{ "ALL" },
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var receiveMessageResponse = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+    foreach (var message in receiveMessageResponse.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(receiveMessageRequest.QueueUrl, message.ReceiptHandle, cts.Token);
+    }
+}
