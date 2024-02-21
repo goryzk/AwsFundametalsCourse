1bad4104c7d69e3f067bbfe511b2c76a369cdf9d
gyezekyan13 <gyezekyan13@gmail.com>
2024-02-21 23:59:08 +0400

    added sns consumer and publisher

---
+++ SnsConsumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var queueName = args.Length == 1 ? args[0] : "customers";
+
+var cts = new CancellationTokenSource();
+var sqsClient = new AmazonSQSClient(RegionEndpoint.USEast1);
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync(queueName);
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string>{ "ALL" },
+    MessageAttributeNames = new List<string>{ "ALL" },
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var receiveMessageResponse = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+    foreach (var message in receiveMessageResponse.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(receiveMessageRequest.QueueUrl, message.ReceiptHandle, cts.Token);
+    }
+}
---
+++ SnsPublisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+
+var customer = new CustomerCreated
+{
+    Email = "gyezekyan13@gmail.com",
+    Id = Guid.NewGuid(),
+    Name = "Gor"
+};
+
+var snsClient = new AmazonSimpleNotificationServiceClient(RegionEndpoint.USEast1);
+
+var topicArnResponse = await snsClient.FindTopicAsync("customers");
+
+var publishRequest = new PublishRequest
+{
+    TopicArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
