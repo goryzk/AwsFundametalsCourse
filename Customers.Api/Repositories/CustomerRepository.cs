3e20332c0bff9785a35645404f0c6ab6c1e2f257
gyezekyan13 <gyezekyan13@gmail.com>
2024-02-21 18:07:51 +0400

    added Database connections

--- Customers.Api/Repositories/CustomerRepository.cs
+++ Customers.Api/Repositories/CustomerRepository.cs
@@ -4,18 +4,11 @@ using Dapper;
 
 namespace Customers.Api.Repositories;
 
-public class CustomerRepository : ICustomerRepository
+public class CustomerRepository(IDbConnectionFactory connectionFactory) : ICustomerRepository
 {
-    private readonly IDbConnectionFactory _connectionFactory;
-
-    public CustomerRepository(IDbConnectionFactory connectionFactory)
-    {
-        _connectionFactory = connectionFactory;
-    }
-
     public async Task<bool> CreateAsync(CustomerDto customer)
     {
-        using var connection = await _connectionFactory.CreateConnectionAsync();
+        using var connection = await connectionFactory.CreateConnectionAsync();
         var result = await connection.ExecuteAsync(
             @"INSERT INTO Customers (Id, GitHubUsername, FullName, Email, DateOfBirth) 
             VALUES (@Id, @GitHubUsername, @FullName, @Email, @DateOfBirth)",
@@ -25,20 +18,20 @@ public class CustomerRepository : ICustomerRepository
 
     public async Task<CustomerDto?> GetAsync(Guid id)
     {
-        using var connection = await _connectionFactory.CreateConnectionAsync();
+        using var connection = await connectionFactory.CreateConnectionAsync();
         return await connection.QuerySingleOrDefaultAsync<CustomerDto>(
             "SELECT * FROM Customers WHERE Id = @Id LIMIT 1", new { Id = id });
     }
 
     public async Task<IEnumerable<CustomerDto>> GetAllAsync()
     {
-        using var connection = await _connectionFactory.CreateConnectionAsync();
+        using var connection = await connectionFactory.CreateConnectionAsync();
         return await connection.QueryAsync<CustomerDto>("SELECT * FROM Customers");
     }
 
     public async Task<bool> UpdateAsync(CustomerDto customer)
     {
-        using var connection = await _connectionFactory.CreateConnectionAsync();
+        using var connection = await connectionFactory.CreateConnectionAsync();
         var result = await connection.ExecuteAsync(
             @"UPDATE Customers SET GitHubUsername = @GitHubUsername, FullName = @FullName, Email = @Email, 
                  DateOfBirth = @DateOfBirth WHERE Id = @Id",
@@ -48,7 +41,7 @@ public class CustomerRepository : ICustomerRepository
 
     public async Task<bool> DeleteAsync(Guid id)
     {
-        using var connection = await _connectionFactory.CreateConnectionAsync();
+        using var connection = await connectionFactory.CreateConnectionAsync();
         var result = await connection.ExecuteAsync(@"DELETE FROM Customers WHERE Id = @Id",
             new {Id = id});
         return result > 0;
