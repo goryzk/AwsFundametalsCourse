4b24c8e4053d31fa56edb10209651cde48bebc2c
gyezekyan13 <gyezekyan13@gmail.com>
2024-02-29 22:29:14 +0400

    secrets

---
+++ SecretsManager.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerClient = new AmazonSecretsManagerClient(RegionEndpoint.USEast1);
+
+var secretsRequest = new GetSecretValueRequest
+{
+    SecretId = "ApiKey",
+    VersionStage = "AWSPREVIOUS", //AWSCURRENT
+};
+
+var response = await secretsManagerClient.GetSecretValueAsync(secretsRequest);
+
+Console.WriteLine(response.SecretString);
+Console.ReadKey();
---
+++ Weather.Api/Controllers/WeatherController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Weather.Api.Services;
+
+namespace Weather.Api.Controllers;
+
+[ApiController]
+public class WeatherController(IWeatherService weatherService) : ControllerBase
+{
+    [HttpGet("weather/{city}")]
+    public async Task<IActionResult> GetWeatherForCity(string city)
+    {
+        var weather = await weatherService.GetCurrentWeatherAsync(city);
+        if (weather is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(weather);
+    }
+}
